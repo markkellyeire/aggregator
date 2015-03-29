f8cdb47ff9a2b33a9542f2103bd2e7c1dbb5145c
mark <you@example.com>
2015-03-29 16:43:58 +1100

    First commit, symfony and blog bundle

---
+++ README.md
@@ -0,0 +1,4 @@
+aggregator
+==========
+
+A Symfony project created on March 29, 2015, 2:52 pm.
---
+++ src/Blogger/BlogBundle/BlogBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Blogger\BlogBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BlogBundle extends Bundle
+{
+}
